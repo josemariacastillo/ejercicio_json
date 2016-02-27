fc7f52aef971ead52c7742e84178cd2bb1fb8aab
Jose Maria Castillo <joseccotan10@gmail.com>
2016-02-27 18:59:05 +0100

    subiada ejercicio1 json prueba1

---
+++ ejercicio1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+#Listar el titulo de las películas y la productora
+
+import json
+
+with open("peliculas.json") as fichero:
+	doc=json.load(fichero)
+
+for l in doc["peliculas"]["pelicula"]:
+	print l["titulo"]
+	print l["sinopsis"]
